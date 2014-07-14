99c79ceb7cb252062aad70075356674c2bacee0d
Douglas Hipolito <=>
2014-07-14 01:07:13 -0300

    finishing the basic features test

--- test/spec/native-xhr.js
+++ test/spec/native-xhr.js
@@ -1,29 +1,61 @@
 /*global describe, it */
 'use strict';
 
-(function () {
+describe('XHRListener can listen without libraries', function () {
 
-  describe('Native XHR', function () {
+  var baseDataUrl = 'base/data/',
 
-    it("simple JSON request", function(done) {
-      var doneFn = jasmine.createSpy("success"),
-          doneListener = jasmine.createSpy("success_listener");
+      makeXHRRequest = function (url, callbackDone, abort) {
+        var xhr = new XMLHttpRequest();
 
-      XhrListener("base/data/test.json", function () {
-        console.log(2);
-      });
+        xhr.onreadystatechange = function () {
+          if (this.readyState == this.DONE && callbackDone) {
+            callbackDone.apply(this, arguments);
+          }
+        };
 
-      var xhr = new XMLHttpRequest();
-      xhr.onreadystatechange = function(args) {
-        if (this.readyState == this.DONE) {
-          console.log(this.status);
-          done()
-        }
-      };
+        xhr.open('GET', url);
+        xhr.send();
+      },
 
-      xhr.open("GET", "base/data/test.json");
-      xhr.send();
+      htmlUrl = baseDataUrl + 'test.html',
+      jsonUrl = baseDataUrl + 'test.json';
+
+  describe("a JSON file", function() {
+    var xhrListenerCall;
+
+    beforeEach(function () {
+      xhrListenerCall = jasmine.createSpy('xhrListenerCall');
+      XhrListener.clearRequestsList();
     });
 
+    it('when the request is opened', function (done) {
+      XhrListener(jsonUrl).onopen(xhrListenerCall);
+
+      makeXHRRequest(jsonUrl, function () {
+        expect(xhrListenerCall).toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('when the request is done before its original handler is called', function (done) {
+      XhrListener(jsonUrl).done(xhrListenerCall);
+
+      makeXHRRequest(jsonUrl, function () {
+        expect(xhrListenerCall).toHaveBeenCalled();
+        done();
+      });
+    });
+
+    it('when the request is done after its original handler is called', function (done) {
+      XhrListener(jsonUrl).done(xhrListenerCall).after();
+
+      makeXHRRequest(jsonUrl, function () {
+        expect(xhrListenerCall).not.toHaveBeenCalled();
+        done();
+      });
+    });
   });
-})();
+
+
+});
